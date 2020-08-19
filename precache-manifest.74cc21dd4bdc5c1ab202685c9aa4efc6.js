e4e439e7fbfd61f912c14d0d96182a4e3bb99a2d
alisacwan <acube.ahmed.5@gmail.com>
2020-08-19 02:33:05 -0400

    Updates

---
+++ precache-manifest.74cc21dd4bdc5c1ab202685c9aa4efc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a01b8d922e2262049f11d4f331a8cf07",
+    "url": "/JourneyPage/index.html"
+  },
+  {
+    "revision": "1ab0094540a0f64cdc9e",
+    "url": "/JourneyPage/static/css/main.65553a91.chunk.css"
+  },
+  {
+    "revision": "364affaf63f0297f7e2b",
+    "url": "/JourneyPage/static/js/2.b4b4cc58.chunk.js"
+  },
+  {
+    "revision": "2d4b8b635e7bbb6f506e6b72bfc06396",
+    "url": "/JourneyPage/static/js/2.b4b4cc58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ab0094540a0f64cdc9e",
+    "url": "/JourneyPage/static/js/main.ccac605c.chunk.js"
+  },
+  {
+    "revision": "a8c6fbecb13116ecaca5",
+    "url": "/JourneyPage/static/js/runtime-main.08a7757f.js"
+  }
+]);
